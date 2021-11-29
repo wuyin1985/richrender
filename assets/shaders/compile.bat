4cc00fe7111c8770df817de1393b92df7f836bca
wuyin <wuyin@outlook.com>
2021-11-29 15:23:42 +0800

    delete assets

--- assets/shaders/compile.bat
+++
@@ -1,3 +0,0 @@
-glslc.exe simple_draw_object_vert.glsl -fshader-stage=vertex -o simple_draw_object_vert.spv -DIN_NORMAL
-glslc.exe simple_draw_object_frag.glsl -fshader-stage=fragment -o simple_draw_object_frag.spv -DIN_NORMAL 
-pause
--- assets/shaders/full_screen_trangle_vert.glsl
+++
@@ -1,9 +0,0 @@
-#version 450
-#pragma shader_stage(vertex)
-
-layout (location = 0) out vec2 outUV;
-
-void main() {
-    gl_Position = ubo.light_matrix * push_constants.model * vec4(in_pos, 1.0);
-}
-
